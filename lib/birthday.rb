58d4a1e7578152706679667ef534c9746f199650
Richard Adule <big.richi@outlook.com>
2021-01-26 17:40:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,8 @@
-birthday_kids = {
-  "Timmy" => 9, 
-  "Sarah" => 6, 
-  "Amanda" => 27
-}
+# birthday_kids = {
+#   "Timmy" => 9, 
+#   "Sarah" => 6, 
+#   "Amanda" => 27
+# }
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
@@ -10,4 +10,3 @@ def happy_birthday(birthday_kids)
   end
 end
 
-happy_birthday(birthday_kids)
