4c492c1ae39a9b0b0f77a0ac334625903a262411
Richard Adule <big.richi@outlook.com>
2021-01-26 17:48:57 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,7 @@ suite_e: "Crumpet the Elf"
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
   passengers.each do |key, ele|
      if key == :suite_a && ele[0] == "A"
        winner = ele
